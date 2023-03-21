f5952c9dae177146635a93ce7d85b1435cf590a3
JPMendiola <126528654+JPMendiola@users.noreply.github.com>
2023-03-21 20:05:27 -0300

    Update Ejercicio1B.java

--- Clase3/Clase3/src/Ejercicio1B.java
+++ Clase3/Clase3/src/Ejercicio1B.java
@@ -1,11 +1,32 @@
+import java.util.Arrays;
+
 public class Ejercicio1B {
 
 	public static void main(String[] args) {
 		
-		int [] numeros = {2,4,3};
-		boolean orden = true;
+		int [] numeros = {1, 2, 3};
+		var orden = true;
 		
-		if
+		if(orden == true) {
+			Arrays.sort(numeros);
+		}
 		
+		for ( int num : numeros) {
+		System.out.println(num);
+		}
+		
+		
+		for (int i = 0; i < numeros.length; i++) {
+			for (int j = i + 1; j < numeros.length; j++) {
+				if (numeros[i] < numeros[j]) {  
+					int temp = numeros[i];  
+					numeros[i] = numeros[j];  
+					numeros[j] = temp;  
+				}  
+			}  
+		}
+		for ( int num : numeros) {
+			System.out.println(num);
+			}
 	}
 }
